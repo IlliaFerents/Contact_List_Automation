cf96997a9ce7caa53212cf3bed69871872e4cc5e
Illia Ferents <illiaferents@gmail.com>
2024-01-13 16:15:29 +0200

    create file for handling random contact data

---
+++ cypress/support/helpers/contact_utils.js
@@ -0,0 +1,13 @@
+export const randomContactData = {
+  firstName: "Jake",
+  lastName: "Matt",
+  birthdate: "1970-01-01",
+  email: "jdoe@fake.com",
+  phone: "8005555555",
+  street1: "1 Main St.",
+  street2: "Apartment A",
+  city: "Anytown",
+  stateProvince: "KS",
+  postalCode: "12345",
+  country: "USA",
+};
