e8986bae83a90febda202c92775738a1e653e2b2
Illia Ferents <illiaferents@gmail.com>
2024-01-15 23:23:05 +0200

    add DELETEallContacts custom command

--- cypress/support/helpers/api_commands.js
+++ cypress/support/helpers/api_commands.js
@@ -37,3 +37,35 @@ Cypress.Commands.add("GETcontactByID", (baseURL, id) => {
     followRedirect: false,
   });
 });
+
+Cypress.Commands.add("DELETEallContacts", () => {
+  cy.request({
+    method: "GET",
+    url: "https://thinking-tester-contact-list.herokuapp.com/contacts",
+    headers: {
+      Authorization: `Bearer ${Cypress.env("token")}`,
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    response.body.forEach((contact) => {
+      cy.request({
+        method: "DELETE",
+        url: `https://thinking-tester-contact-list.herokuapp.com/contacts/${contact._id}`,
+        headers: {
+          Authorization: `Bearer ${Cypress.env("token")}`,
+          "Content-Type": "application/json",
+        },
+        failOnStatusCode: false,
+      });
+    });
+  });
+});
+
+Cypress.Commands.add("addContacts", (numOfContacts) => {
+  for (let i = 0; i < numOfContacts; i++) {
+    cy.POSTrequest(
+      "https://thinking-tester-contact-list.herokuapp.com/contacts/",
+      ContactData.validValues
+    );
+  }
+});
