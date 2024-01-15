d9822eb471a36893f4fbdbb5e1c9feafbce2f592
Illia Ferents <illiaferents@gmail.com>
2024-01-15 23:19:06 +0200

    add Contact Search by ID test suite

---
+++ cypress/e2e/API/Contacts/get_contact_by_id.cy.js
@@ -0,0 +1,36 @@
+import * as ContactData from "../../../support/helpers/contact_data.js";
+const contactsApiURL = Cypress.env("contactsApiURL");
+
+describe("Contact Search by ID", () => {
+  before(() => {
+    cy.DELETEallContacts();
+  });
+  it("retrieves a contact by id", function () {
+    cy.addContacts(1).then((response) => {
+      const contactID = response.body._id;
+
+      cy.GETcontactByID(contactsApiURL, contactID).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body._id).to.eq(contactID);
+      });
+    });
+  });
+  it("returns error message for request with invalid id", () => {
+    cy.GETcontactByID(contactsApiURL, "abcde").then((response) => {
+      expect(response.status).to.eq(400);
+      expect(response.body).to.eq("Invalid Contact ID");
+    });
+  });
+  it("returns error message for request with non-exsiting id", () => {
+    cy.GETcontactByID(contactsApiURL, "65a595402b31aa00139c1ff5").then((response) => {
+      expect(response.status).to.eq(404);
+      expect(response.body).to.be.empty;
+    });
+  });
+  it.only("returns empty array for request with special characters in id", () => {
+    cy.GETcontactByID(contactsApiURL, "#?/.").then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.deep.equal([]);
+    });
+  });
+});
