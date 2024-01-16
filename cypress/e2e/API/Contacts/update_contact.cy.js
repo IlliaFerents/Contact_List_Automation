4c9a68d4dd179bd2c3e123ddede966715e009723
Illia Ferents <illiaferents@gmail.com>
2024-01-16 23:53:15 +0200

    add Contact Update test suite

---
+++ cypress/e2e/API/Contacts/update_contact.cy.js
@@ -0,0 +1,28 @@
+import * as ContactData from "../../../support/helpers/contact_data.js";
+import { assertErrorMessagesAPI } from "../../../support/helpers/assertions.js";
+const { faker } = require("@faker-js/faker");
+
+const contactsApiURL = Cypress.env("contactsApiURL");
+
+describe("Contact Creation", () => {
+  before(() => {
+    cy.addContacts(3).then((createdContacts) => {
+      cy.wrap(createdContacts[0]._id).as("contactOneID");
+      cy.wrap(createdContacts[1]._id).as("contactTwoID");
+      cy.wrap(createdContacts[2]._id).as("contactThreeID");
+    });
+  });
+  it("updates an existing contact with random data", function () {
+    cy.GETcontactByID(contactsApiURL, this.contactOneID).then((response) => {
+      const originalContactData = response.body;
+
+      cy.updateContactDataByID(contactsApiURL, ContactData.validValues, this.contactOneID).then(
+        (response) => {
+          expect(response.status).to.eq(200);
+          expect(response.body).not.to.deep.equal(originalContactData);
+          expect(response.body).to.deep.include(ContactData.validValues);
+        }
+      );
+    });
+  });
+});
