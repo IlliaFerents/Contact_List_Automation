32d0471d2e7b9f8a3f601245ad5b5a236615c9ab
Illia Ferents <illiaferents@gmail.com>
2024-01-16 19:45:47 +0200

    add custom API commands for deleting contacts

--- cypress/support/helpers/api_commands.js
+++ cypress/support/helpers/api_commands.js
@@ -40,6 +40,34 @@ Cypress.Commands.add("GETcontactByID", (baseURL, id) => {
   });
 });
 
+Cypress.Commands.add("DELETEcontactByID", (baseURL, id) => {
+  cy.request({
+    method: "DELETE",
+    url: baseURL + id,
+    headers: {
+      Authorization: `Bearer ${Cypress.env("token")}`,
+      "Content-Type": "application/json",
+    },
+    failOnStatusCode: false,
+    followRedirect: false,
+  });
+});
+
+Cypress.Commands.add("DELETEmultipleContactsByID", (baseURL, ids) => {
+  ids.forEach((id) => {
+    cy.request({
+      method: "DELETE",
+      url: baseURL + id,
+      headers: {
+        Authorization: `Bearer ${Cypress.env("token")}`,
+        "Content-Type": "application/json",
+      },
+      failOnStatusCode: false,
+      followRedirect: false,
+    });
+  });
+});
+
 Cypress.Commands.add("DELETEallContacts", () => {
   cy.request({
     method: "GET",
@@ -64,10 +92,15 @@ Cypress.Commands.add("DELETEallContacts", () => {
 });
 
 Cypress.Commands.add("addContacts", (numOfContacts) => {
+  const createdContacts = [];
+
   for (let i = 0; i < numOfContacts; i++) {
     cy.POSTrequest(
       "https://thinking-tester-contact-list.herokuapp.com/contacts/",
       ContactData.validValues
-    );
+    ).then((response) => {
+      createdContacts.push(response.body);
+    });
   }
+  return cy.wrap(createdContacts);
 });
