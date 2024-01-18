d5a2eecf6133eaf9cad8a7f3aa0d2689898b2277
Illia Ferents <illiaferents@gmail.com>
2024-01-18 21:27:24 +0200

    rename contact_data to contact_data_helper

--- cypress/support/helpers/contact_data_helper.js
+++ cypress/support/helpers/contact_data_helper.js
@@ -1,18 +1,22 @@
 const { faker } = require("@faker-js/faker");
 
-export const validValues = {
-  firstName: faker.person.firstName(),
-  lastName: faker.person.lastName(),
-  birthdate: faker.date.birthdate().toISOString().split("T")[0], // yyyy-mm-dd
-  email: faker.internet.email().toLowerCase(),
-  phone: faker.string.numeric(8),
-  street1: faker.location.streetAddress(),
-  street2: faker.location.streetAddress(),
-  city: faker.location.city(),
-  stateProvince: faker.location.state(),
-  postalCode: faker.location.zipCode(),
-  country: faker.location.country(),
+export const generateValidValues = () => {
+  return {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    birthdate: faker.date.birthdate().toISOString().split("T")[0], // yyyy-mm-dd
+    email: faker.internet.email().toLowerCase(),
+    phone: faker.string.numeric(8),
+    street1: faker.location.streetAddress(),
+    street2: faker.location.streetAddress(),
+    city: faker.location.city(),
+    stateProvince: faker.location.state(),
+    postalCode: faker.location.zipCode(),
+    country: faker.location.country(),
+  };
 };
+
+export const validValues = generateValidValues();
 
 export const maxLenValues = {
   firstName: faker.string.alpha(21),
