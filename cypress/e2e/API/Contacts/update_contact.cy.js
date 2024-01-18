160143c7b53b91da221c96f1a2110e9545150d89
Illia Ferents <illiaferents@gmail.com>
2024-01-18 21:28:10 +0200

    add negative tests to Contact Update test suite

--- cypress/e2e/API/Contacts/update_contact.cy.js
+++ cypress/e2e/API/Contacts/update_contact.cy.js
@@ -1,5 +1,5 @@
-import * as ContactData from "../../../support/helpers/contact_data.js";
-import { assertErrorMessagesAPI } from "../../../support/helpers/assertions.js";
+import * as ContactData from "../../../support/helpers/contact_data_helper.js";
+import { assertAPIerrorMessages } from "../../../support/helpers/assertions.js";
 const { faker } = require("@faker-js/faker");
 
 const contactsApiURL = Cypress.env("contactsApiURL");
@@ -7,36 +7,90 @@ const contactsApiURL = Cypress.env("contactsApiURL");
 describe("Contact Update", () => {
   before(() => {
     cy.deleteAllContacts();
-    cy.addMultipleContacts(3).then((createdContacts) => {
-      cy.wrap(createdContacts[0]._id).as("contactOneID");
-      cy.wrap(createdContacts[1]._id).as("contactTwoID");
-      cy.wrap(createdContacts[2]._id).as("contactThreeID");
+  });
+  beforeEach(() => {
+    cy.addMultipleContacts(1).then((createdContact) => {
+      cy.wrap(createdContact[0]._id).as("contactID");
     });
   });
   it("updates an existing contact with random data", function () {
-    cy.getContactByID(this.contactOneID).then((response) => {
+    cy.getContactByID(this.contactID).then((response) => {
       const originalContactData = response.body;
 
-      cy.updateContactByID(ContactData.validValues, this.contactOneID).then((response) => {
+      cy.updateContactByID(ContactData.validValues, this.contactID).then((response) => {
         expect(response.status).to.eq(200);
         expect(response.body).not.to.deep.equal(originalContactData);
 
-        cy.getContactByID(this.contactOneID).then((response) => {
+        cy.getContactByID(this.contactID).then((response) => {
           expect(response.body).to.deep.include(ContactData.validValues);
         });
       });
     });
   });
   it("updates an existing contact with incomplete data", function () {
-    cy.getContactByID(this.contactTwoID).then((response) => {
+    cy.getContactByID(this.contactID).then((response) => {
       const originalContactData = response.body;
 
-      cy.updateContactByID(ContactData.requiredOnlyFields, this.contactTwoID).then((response) => {
+      cy.updateContactByID(ContactData.requiredOnlyFields, this.contactID).then((response) => {
         expect(response.status).to.eq(200);
         expect(response.body).not.to.deep.equal(originalContactData);
 
-        cy.getContactByID(this.contactTwoID).then((response) => {
+        cy.getContactByID(this.contactID).then((response) => {
           expect(response.body).to.deep.include(ContactData.requiredOnlyFields);
+        });
+      });
+    });
+  });
+  it("returns error messages for updating existing contact with missing required fields", function () {
+    cy.getContactByID(this.contactID).then((response) => {
+      const originalContactData = response.body;
+
+      cy.updateContactByID(
+        {
+          email: ContactData.validValues.email,
+          phone: ContactData.validValues.phone,
+        },
+        this.contactID
+      ).then((response) => {
+        expect(response.status).to.eq(400);
+        assertAPIerrorMessages(response, {
+          lastName: "Path `lastName` is required.",
+          firstName: "Path `firstName` is required.",
+        });
+      });
+    });
+  });
+  it("returns error messages for updating existing contact with invalid data", function () {
+    cy.getContactByID(this.contactID).then((response) => {
+      const originalContactData = response.body;
+
+      cy.updateContactByID(ContactData.invalidValues, this.contactID).then((response) => {
+        expect(response.status).to.eq(400);
+        assertAPIerrorMessages(response, {
+          email: "Email is invalid",
+          phone: "Phone number is invalid",
+          birthdate: "Birthdate is invalid",
+          postalCode: "Postal code is invalid",
+        });
+      });
+    });
+  });
+  it("returns error messages for request with invalid keys", function () {
+    cy.getContactByID(this.contactID).then((response) => {
+      const originalContactData = response.body;
+
+      cy.updateContactByID(
+        {
+          firstname: ContactData.validValues.firstName,
+          lastname: ContactData.validValues.lastName,
+          Phone: ContactData.validValues.phone,
+        },
+        this.contactID
+      ).then((response) => {
+        expect(response.status).to.eq(400);
+        assertAPIerrorMessages(response, {
+          lastName: "Path `lastName` is required.",
+          firstName: "Path `firstName` is required.",
         });
       });
     });
