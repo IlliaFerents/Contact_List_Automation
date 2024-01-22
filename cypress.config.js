223685a925cde6e3e7b6e73d669970157af72904
Illia Ferents <illiaferents@gmail.com>
2024-01-22 23:42:26 +0200

    move environmental variables to .env

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require("cypress");
+require("dotenv").config();
 
 module.exports = defineConfig({
   projectId: "g6at4p",
   e2e: {
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
-      require("@bahmutov/cy-grep/src/plugin")(config);
+      require("@cypress/grep/src/plugin")(config);
       return config;
     },
   },
-  env: { grepFilterSpecs: true, grepOmitFiltered: true },
+  env: { ...process.env, grepFilterSpecs: true, grepOmitFiltered: true },
 });
