224e1ac2a7ed59336117bf820ab4e8c6734864c1
Illia Ferents <illiaferents@gmail.com>
2024-01-22 23:45:07 +0200

    add new import to e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,7 +16,8 @@
 // Import commands.js using ES2015 syntax:
 import "./commands";
 import "./helpers/api_commands";
-import registerCypressGrep from "@bahmutov/cy-grep/src/support";
+import "./helpers/contact_api_commands";
+import registerCypressGrep from "@cypress/grep/src/support";
 registerCypressGrep();
 
 // Alternatively you can use CommonJS syntax:
