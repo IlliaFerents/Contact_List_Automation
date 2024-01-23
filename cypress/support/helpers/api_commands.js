03ee7ebf83f69234c7c8fcf800f676e7b4212f8d
Illia Ferents <illiaferents@gmail.com>
2024-01-23 13:49:08 +0200

    create ApiRequest class with request method for handling other api commands

--- cypress/support/helpers/api_commands.js
+++ cypress/support/helpers/api_commands.js
@@ -1,13 +1,19 @@
-Cypress.Commands.add("apiRequest", (method, baseUrl, url = "", payload) => {
-  cy.request({
-    method: method,
-    url: baseUrl + url,
-    body: payload,
-    headers: {
-      Authorization: `Bearer ${Cypress.env("BEARER_TOKEN")}`,
-      "Content-Type": "application/json",
-    },
-    failOnStatusCode: false,
-    followRedirect: false,
-  });
-});
+export default class ApiRequest {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  request(method, url = "", payload) {
+    return cy.request({
+      method: method,
+      url: this.baseUrl + url,
+      body: payload,
+      headers: {
+        Authorization: `Bearer ${Cypress.env("BEARER_TOKEN")}`,
+        "Content-Type": "application/json",
+      },
+      failOnStatusCode: false,
+      followRedirect: false,
+    });
+  }
+}
