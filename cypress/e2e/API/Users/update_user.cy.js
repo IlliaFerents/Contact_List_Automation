a77df3b443b18e4d243d2df4182f6c94077f316b
Illia Ferents <illiaferents@gmail.com>
2024-01-29 18:53:52 +0200

    refactor data creation to be accessed via aliases

--- cypress/e2e/API/Users/update_user.cy.js
+++ cypress/e2e/API/Users/update_user.cy.js
@@ -4,33 +4,45 @@ import { assertAPIerrorMessages } from "../../../support/helpers/assertions.js";
 describe("User Update", { tags: ["@api", "@user"] }, () => {
   context("PATCH /users/me", () => {
     beforeEach(function () {
-      cy.addUser(UserData.generateValidValues()).then((response) => {
+      const validPayload = UserData.generateValidValues();
+      const updatePayload = UserData.generateValidValues();
+
+      cy.wrap(validPayload).as("validPayload");
+      cy.wrap(updatePayload).as("updatePayload");
+
+      cy.addUser(validPayload).then((response) => {
         cy.wrap(response.body.token).as("userToken");
-        cy.wrap(response.body.user).as("postPayload");
+        cy.wrap(response.body.user).as("originalUserData");
       });
     });
     it("updates an existing user with random data", { tags: ["@smoke"] }, function () {
-      cy.updateUser(UserData.generateValidValues(), this.userToken).then((response) => {
-        const updatePayload = response.body;
+      cy.updateUser(this.updatePayload, this.userToken).then((response) => {
+        const updatedUserData = response.body;
+
         expect(response.status).to.eq(200);
-        expect(updatePayload).not.to.deep.equal(this.postPayload);
+        expect(updatedUserData).not.to.deep.equal(this.originalUserData);
 
         cy.getUser(this.userToken).then((response) => {
           expect(response.status).to.eq(200);
-          expect(response.body).to.deep.equal(updatePayload);
+          expect(response.body).to.deep.equal(updatedUserData);
         });
       });
     });
     it("partially updates an existing user", function () {
-      cy.updateUser({ email: UserData.validValues.email }, this.userToken).then((response) => {
-        const updatePayload = response.body;
+      cy.updateUser({ email: this.updatePayload.email }, this.userToken).then((response) => {
+        const updatedUserData = response.body;
+
         expect(response.status).to.eq(200);
-        expect(updatePayload.email).to.equal(UserData.validValues.email);
-        expect(updatePayload).not.to.deep.equal(this.postPayload);
+        expect(updatedUserData.email).to.equal(this.updatePayload.email);
+        expect(updatedUserData.email).not.to.eq(this.originalUserData.email);
+        expect(updatedUserData).to.deep.include({
+          firstName: this.originalUserData.firstName,
+          lastName: this.originalUserData.lastName,
+        });
 
         cy.getUser(this.userToken).then((response) => {
           expect(response.status).to.eq(200);
-          expect(response.body).to.deep.equal(updatePayload);
+          expect(response.body).to.deep.equal(updatedUserData);
         });
       });
     });
@@ -43,7 +55,7 @@ describe("User Update", { tags: ["@api", "@user"] }, () => {
 
         cy.getUser(this.userToken).then((response) => {
           expect(response.status).to.eq(200);
-          expect(response.body).to.deep.equal(this.postPayload);
+          expect(response.body).to.deep.equal(this.originalUserData);
         });
       });
     });
