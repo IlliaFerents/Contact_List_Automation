02f4f087d9f9e7605ca3eaf06772a300db3842b4
Illia Ferents <illiaferents@gmail.com>
2024-02-01 22:51:08 +0200

    refactor user data

--- cypress/support/helpers/random_data/user_data_helper.ts
+++ cypress/support/helpers/random_data/user_data_helper.ts
@@ -19,22 +19,22 @@ export const generateValidValues = (): UserData => {
 export const validValues: UserData = generateValidValues();
 
 export const invalidValues: UserData = {
-  firstName: faker.string.alpha(5),
-  lastName: faker.string.alpha(5),
+  firstName: faker.string.alpha(5), // accepts any value
+  lastName: faker.string.alpha(5), // accepts any value
   email: faker.string.alpha(5),
-  password: faker.internet.password(),
+  password: faker.internet.password(), // accepts any value
 };
 
-export const maxMinLenValues: UserData = {
+export const invalidLengthValues: UserData = {
   firstName: faker.string.alpha(21),
   lastName: faker.string.alpha(21),
-  email: faker.internet.email(),
+  email: faker.internet.email(), // accepts any length
   password: faker.string.alpha({ length: { min: 1, max: 6 } }),
 };
 
-export const invalidKeys: UserData = {
-  firstName: faker.person.firstName(),
-  lastName: faker.person.lastName(),
-  email: faker.internet.email().toLowerCase(),
-  password: faker.internet.password(),
+export const invalidKeys = {
+  firstname: faker.person.firstName(),
+  lastname: faker.person.lastName(),
+  Email: faker.internet.email().toLowerCase(),
+  Password: faker.internet.password(),
 };
