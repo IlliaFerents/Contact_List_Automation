54e6f3a921f495f9403ad390d627014a05ac3c6b
Illia Ferents <illiaferents@gmail.com>
2024-02-01 22:34:02 +0200

    refactor

--- cypress/support/helpers/commands/users_api_commands.ts
+++ cypress/support/helpers/commands/users_api_commands.ts
@@ -6,7 +6,7 @@ Cypress.Commands.add("addUser", (payload) => {
   api.request("POST", "", payload);
 });
 
-Cypress.Commands.add("getUser", (token?) => {
+Cypress.Commands.add("getUser", (token) => {
   api.request("GET", "me", "", token);
 });
 Cypress.Commands.add("updateUser", (payload, token) => {
