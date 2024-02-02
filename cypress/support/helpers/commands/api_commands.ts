9721bbe42631a8e6be988faa6f7ef4db9594d97c
Illia Ferents <illiaferents@gmail.com>
2024-02-02 23:57:16 +0200

    update ApiRequest class with logRequest and logResponse methods

--- cypress/support/helpers/commands/api_commands.ts
+++ cypress/support/helpers/commands/api_commands.ts
@@ -1,20 +1,62 @@
 export default class ApiRequest {
   baseUrl: string;
+
   constructor(baseUrl: string) {
     this.baseUrl = baseUrl;
   }
 
-  request(method: string, url: string = "", payload?, token: string = Cypress.env("BEARER_TOKEN")) {
-    return cy.request({
-      method: method,
-      url: this.baseUrl + url,
-      body: payload,
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-      failOnStatusCode: false,
-      followRedirect: false,
+  logRequest(method: string, url: string, payload?) {
+    const fullUrl = new URL(this.baseUrl + url);
+
+    Cypress.log({
+      name: "API Request",
+      displayName: "API Req:",
+      message: `${method} ${fullUrl.pathname}`,
+      consoleProps: () => ({
+        Method: method,
+        URL: `${fullUrl}`,
+        Payload: payload,
+      }),
     });
+  }
+
+  logResponse(method: string, url: string, response) {
+    const isSuccess = response.status >= 200 && response.status < 400;
+    const statusIcon = isSuccess ? "🟢" : "🔴";
+    const fullUrl = new URL(this.baseUrl + url);
+
+    Cypress.log({
+      name: "API Response",
+      displayName: "API Res:",
+      message: `${statusIcon} ${method} ${response.status}`,
+      consoleProps: () => ({
+        Method: method,
+        URL: `${fullUrl}`,
+        Status: response.status,
+        Response: response.body,
+      }),
+    });
+  }
+
+  request(method: string, url: string = "", payload?, token: string = Cypress.env("BEARER_TOKEN")) {
+    this.logRequest(method, url, payload);
+
+    return cy
+      .request({
+        method: method,
+        url: this.baseUrl + url,
+        body: payload,
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        failOnStatusCode: false,
+        followRedirect: false,
+        log: false,
+      })
+      .then((response) => {
+        this.logResponse(method, url, response);
+        return response;
+      });
   }
 }
