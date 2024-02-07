5b7ea585369eddaaef61eaef30fe650aaf39cbcb
Illia Ferents <illiaferents@gmail.com>
2024-02-08 00:17:27 +0200

    add assertContactInfo assertion function

--- cypress/support/helpers/assertions.ts
+++ cypress/support/helpers/assertions.ts
@@ -9,3 +9,15 @@ export function assertAPIerrorMessages(response, expectedErrors: object) {
   const expectedErrorMessage = "validation failed:";
   expect(response.body.message.toLowerCase()).to.include(expectedErrorMessage);
 }
+
+export function assertContactInfo(actual, expected) {
+  expect(actual.fullName).to.equal(expected.firstName + " " + expected.lastName);
+  expect(actual.birthDate).to.equal(expected.birthdate);
+  expect(actual.email).to.equal(expected.email);
+  expect(actual.phoneNumber).to.equal(expected.phone);
+  expect(actual.address).to.equal(expected.street1 + " " + expected.street2);
+  expect(actual.city).to.equal(
+    expected.city + " " + expected.stateProvince + " " + expected.postalCode
+  );
+  expect(actual.country).to.equal(expected.country);
+}
