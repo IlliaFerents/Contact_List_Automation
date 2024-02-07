ff72b24115a2beb93e449b51af82e203fea714eb
Illia Ferents <illiaferents@gmail.com>
2024-02-08 00:15:49 +0200

    add custom login commands

--- cypress/support/helpers/commands/ui_commands.ts
+++ cypress/support/helpers/commands/ui_commands.ts
@@ -1,3 +1,28 @@
 Cypress.Commands.add("getByID", (idValue, ...args) => {
   return cy.get(`#${idValue}`, ...args);
 });
+
+Cypress.Commands.add("login", (email, password) => {
+  cy.session(
+    [email, password],
+    () => {
+      cy.loginByApi(email, password);
+    },
+    {
+      cacheAcrossSpecs: true,
+      validate() {
+        cy.getCookie("token").should("exist");
+      },
+    }
+  );
+});
+
+Cypress.Commands.add("loginWithGeneratedUser", () => {
+  const validUserData = UserData.generateValidValues();
+
+  cy.addUser(validUserData).then((createdUser) => {
+    const email = createdUser.body.user.email;
+    const password = validUserData.password;
+    cy.login(email, password);
+  });
+});
